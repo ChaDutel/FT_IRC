4f923120a968a7b277f6fdf6451b7cb0312dee07
Loic Johnson <ljohnson@z4r4p5.42lyon.fr>
2023-06-09 14:59:31 +0200

    Exceptions: 	Added UserAlreadyOperator 	Added UserAlreadyInChannel 	Added CannotKickOperator 	harmonized syntax

--- srcs/Exceptions.cpp
+++ srcs/Exceptions.cpp
@@ -6,17 +6,20 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 15:49:42 by ljohnson          #+#    #+#             */
-/*   Updated: 2023/06/07 15:48:13 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2023/06/09 14:57:55 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Exceptions.hpp>
 
-char const*	SocketFailException::what()		const throw() {return ("ERROR: Socket Failed");}
-char const*	SetSockOptFailException::what()	const throw() {return ("ERROR: SetSockOpt Failed");}
-char const*	BindFailException::what()		const throw() {return ("ERROR: Bind Failed");}
-char const*	ListenFailException::what()		const throw() {return ("ERROR: Listen Failed");}
-char const*	ServerClosedException::what()	const throw() {return ("ERROR: Server closed");}
-char const*	SelectFailException::what()		const throw() {return ("ERROR: Select Failed");}
-char const*	AcceptFailException::what()		const throw() {return ("ERROR: Accept Failed");}
-char const*	SigIntException::what()			const throw() {return ("SIGINT called, server stopped.");}
+char const*	SocketFailException::what()				const throw() {return ("ERROR: Socket Failed");}
+char const*	SetSockOptFailException::what()			const throw() {return ("ERROR: SetSockOpt Failed");}
+char const*	BindFailException::what()				const throw() {return ("ERROR: Bind Failed");}
+char const*	ListenFailException::what()				const throw() {return ("ERROR: Listen Failed");}
+char const*	ServerClosedException::what()			const throw() {return ("ERROR: Server closed");}
+char const*	SelectFailException::what()				const throw() {return ("ERROR: Select Failed");}
+char const*	AcceptFailException::what()				const throw() {return ("ERROR: Accept Failed");}
+char const*	SigIntException::what()					const throw() {return ("SIGINT called, server stopped.");}
+char const*	UserAlreadyOperatorException::what()	const throw() {return ("ERROR: User already Operator in this channel");}
+char const*	UserAlreadyInChannelException::what()	const throw() {return ("ERROR: User is already in channel");}
+char const*	CannotKickOperatorException::what()		const throw() {return ("ERROR: Cannot kick an operator");}
